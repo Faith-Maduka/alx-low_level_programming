5bbd53c76576cbe43eac4ce8f0bbab83765d605c
Faith-Maduka <faithdavies8@gmail.com>
2021-06-17 12:39:37 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,9 +3,9 @@
  *main - primary function serves as starting point of the program
  *Return: 0 upon the reaching the end of the program
  */
- int main(void)
- {
-   char str[] = "\"Programming is like building a multilingual puzzle";
-   puts(str);
-   return (0);
- }
+int main(void)
+{
+char str[] = "\"Programming is like building a multilingual puzzle";
+puts(str);
+return (0);
+}
