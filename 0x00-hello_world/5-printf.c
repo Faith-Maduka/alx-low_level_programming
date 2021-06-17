5224eb0635ca2d58ddf5365e08f27a417db1b3eb
Faith-Maduka <faithdavies8@gmail.com>
2021-06-17 12:46:04 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - primary function serves as starting point of the program
+ *Return: 0 upon the reaching the end of the program
+ */
+int main(void)
+{
+  char str[] = "\"Programming is like building a multilingual puzzle";
+  printf(str);
+  return (0);
+}
