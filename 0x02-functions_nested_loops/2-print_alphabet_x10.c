4d6efb0c18d1dccc9eb7abf5ae5ca0a9a411139d
Faith-Maduka <faithdavies8@gmail.com>
2021-06-22 06:33:57 -0700

    alpha

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,23 +1,24 @@
 #include "holberton.h"
+
 /**
- *print_alphabet_x10 - prints alphabets 10
- *
- *Description: prints 10 times
- *Return: Always(0).
+ * print_alphabet_x10 - prints alphabet 10 times
+ * Return:void
  */
 
 void print_alphabet_x10(void)
 {
-int alphabet_count = 0;
-while (alphabet_count < 10)
+char c;
+int i = 0;
+
+while (i < 10)
 {
-char ch = 'a';
-while (ch <= 'z')
+c = 'a';
+while (c <= 'z')
 {
-_putchar(ch);
-ch++
+_putchar(c);
+c++;
 }
-alphabet_count++;
 _putchar('\n');
+i++;
 }
 }
