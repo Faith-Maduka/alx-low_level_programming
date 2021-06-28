3d1156f6b9b1a9ce98ca0d4b4458a72e1d51f947
Faith-Maduka <faithdavies8@gmail.com>
2021-06-28 02:49:00 -0700

    4
    git add .
    git commit -m 4

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,25 +1,19 @@
 #include "holberton.h"
 
 /**
-* print_rev - print reverse
-* @s: input string
-*
-* Description:  prints a string, in reverse
-* Return: Always (0)
+* print_rev - prints reversed string, followed by a new line
+* @s: pointer to the string to print
+* Return: void
 */
 
 void print_rev(char *s)
 {
-int left, len, i;
-len = 0;
-left = 0;
-for (i = 0; s[i] != '\0'; ++i)
+int i = 0;
+while (s[i])
+i++;
+while (i--)
 {
-len = i;
-}
-for (; len >= left; len--)
-{
-_putchar(s[len]);
+_putchar(s[i]);
 }
 _putchar('\n');
 }
