d8e7fd4f005235582784bc9176a30a972f340ba0
Faith-Maduka <faithdavies8@gmail.com>
2021-08-05 13:37:38 -0700

    4

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,19 +1,41 @@
 #include "main.h"
 
 /**
- * clear_bit - Sets the value of a bit at a given index to 0.
- * @n: A pointer to the bit.
- * @index: The index to set the value at - indices start at 0.
- *
- * Return: If an error occurs - -1.
- *         Otherwise - 1.
+ * _to_power - powers a number b to the p's power
+ * @base: base inupt
+ * @power: power
+ * Return: return b to the power of a
  */
+
+unsigned long int _to_power(int base, int power)
+{
+	unsigned long int res = 1;
+
+	while (power)
+	{
+		res *= base;
+		power--;
+	}
+	return (res);
+}
+
+/**
+ * clear_bit - sets bit to zero at index index
+ * @n: input integer
+ * @index: returns the value of a bit at a given index
+ * Return: 1 for ssucess -1 for failure
+ */
+
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= (sizeof(unsigned long int) * BYTE_LENGHT))
+	unsigned long int test;
+
+	if (index > sizeof(n) * 8 - 1)
+	{
 		return (-1);
-
-	*n &= ~(1 << index);
-
+	}
+	test = _to_power(2, index);
+	*n = (*n & test) ? *n ^ test : *n;
 	return (1);
+
 }
