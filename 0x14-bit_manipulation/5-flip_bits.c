275f728c8801d1a80656ddf180c02079e69650db
Faith-Maduka <faithdavies8@gmail.com>
2021-08-05 13:26:48 -0700

    files

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
  * flip_bits - number of different bits between two numbers
@@ -10,17 +10,17 @@
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int diff, check;
-	unsigned int count, i;
+unsigned long int diff, check;
+unsigned int count, i;
 
-	count = 0;
-	check = 1;
-	diff = n ^ m;
-	for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
-	{
-		if (check == (diff & check))
-			count++;
-		check <<= 1;
-	}
-	return (count);
+count = 0;
+check = 1;
+diff = n ^ m;
+for (i = 0; i < (sizeof(unsigned long int) * 8); i++)
+{
+if (check == (diff & check))
+count++;
+check <<= 1;
+}
+return (count);
 }
