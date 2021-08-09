af8bce680e46754481ac15cbd78bf219b70dde68
Faith-Maduka <faithdavies8@gmail.com>
2021-08-09 13:18:59 -0700

    100

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,40 +1,18 @@
-#include "holberton.h"
+#include <stdio.h>
 #include "main.h"
-
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
 /**
- * main-displays the information contained in the ELF header at the start of ELF file
- * @argc: argument count
- * @argv: argument variable
- * Return: 0
+ * main - Copies the content of a file to another file.
+ * @ac: Argument count
+ * @av: argument values
+ * Return: 0 on succes, -1 on error.
  */
-
-int main(int argc, char *argv[])
+int main(int ac, char *av[])
 {
-int dest;
-ssize_t size;
-char buffer[5];
-char elf[1];
-if (argc != 2)
-{
-dprintf(STDERR_FILENO, "Usage: cp file\n");
-exit(97);
-}
-dest = open(argv[1], O_RDWR);
-if (dest == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't open from file %s\n", argv[1]);
-exit(98);
-}
-size = read(dest, buffer, 4);
-if (size == -1)
-{
-dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-exit(98);
-}
-size = lseek(dest, 1, SEEK_SET);
-size = read(dest, elf, 1);
-if (elf[0] != 'E')
-exit(98);
-close(dest);
-return (0);
+	return (1);
+	return (0);
 }
