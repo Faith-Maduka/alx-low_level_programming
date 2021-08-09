090183dcca9209e18eacaec1fe16544f40a5e2e3
Faith-Maduka <faithdavies8@gmail.com>
2021-08-09 10:37:23 -0700

    0

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -1,47 +1,27 @@
+#include "holberton.h"
 #include "main.h"
 
 /**
- * read_textfile - a function that reads a
- * text file and prints it to the POSIX standard output
- * @filename: of the file
- * @letters: the number of letters it should read and print
- * Return: the actual number of letters it could read and print
+ * read_textfile - reads a textfile and prints to the POSIX standard output
+ * @filename: file to read and print
+ * @letters: number of letters to print
+ * Return: actual number of letter
  */
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	int s, t;
-	char *buf;
-
-	if (!filename)
-		return (0);
-
-	fd = open(filename, O_RDONLY);
-	if (fd < 0)
-		return (0);
-
-	buf = malloc(sizeof(char) * letters);
-	if (!buf)
-		return (0);
-
-	s = read(fd, buf, letters);
-	if (s < 0)
-	{
-		free(buf);
-		return (0);
-	}
-	buf[s] = '\0';
-
-	close(fd);
-
-	t = write(STDOUT_FILENO, buf, s);
-	if (t < 0)
-	{
-		free(buf);
-		return (0);
-	}
-
-	free(buf);
-	return (t);
+int dest, size;
+char *buffer;
+if (filename == NULL)
+return (0);
+buffer = malloc(sizeof(char) * letters);
+if (buffer == NULL)
+return (0);
+dest = open(filename, O_RDONLY);
+size = write(STDOUT_FILENO, buffer, read(dest, buffer, letters));
+if (dest == -1 || size == -1)
+return (0);
+close(dest);
+free(buffer);
+return (size);
 }
